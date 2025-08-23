d967653570d898d83cc0bb6e93348b3ea5b3418c
varruunnn <varun.choudhary2512@gmail.com>
2025-08-24 00:40:11 +0530

    new project added

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -17,6 +17,7 @@ export const projects = [
     ],
     img: "/classSync.png",
     color: "#16a34a",
+    demoLink: "https://class-sync-sandy.vercel.app",
     githubLink: "https://github.com/varruunnn/ClassSync",
     featured: true,
   },
